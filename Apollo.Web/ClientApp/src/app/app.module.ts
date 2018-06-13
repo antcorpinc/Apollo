6f55473372dc5c1d0bcc4b52800dd72d4c221430
Roni <antcorpinc@gmail.com>
2018-06-13 05:59:06 +0530

    Adding Material Module + Shared

--- Apollo.Web/ClientApp/src/app/app.module.ts
+++ Apollo.Web/ClientApp/src/app/app.module.ts
@@ -2,14 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FwModule } from './framework/fw/fw.module';
 import { AppComponent } from './app.component';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from './common/material/material.module';
+import {SharedModule} from './common/shared/shared.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    FwModule
+    BrowserAnimationsModule,
+    FwModule,
+    SharedModule,
+    MaterialModule
+    // Routing Module should be the last.
   ],
   providers: [],
   bootstrap: [AppComponent]
