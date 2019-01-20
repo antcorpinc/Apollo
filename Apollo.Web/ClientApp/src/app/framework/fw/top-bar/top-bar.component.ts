dd52d0846c2fd361396c0168eb948b6941a0314e
Roni <antcorpinc@gmail.com>
2019-01-20 16:25:19 +0530

    Logout

--- Apollo.Web/ClientApp/src/app/framework/fw/top-bar/top-bar.component.ts
+++ Apollo.Web/ClientApp/src/app/framework/fw/top-bar/top-bar.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, OnChanges } from '@angular/core';
+import { Component, OnInit, OnChanges, ViewChild } from '@angular/core';
 import { FrameworkConfigService } from '../services/framework-config.service';
 import { TopBarService } from '../services/top-bar.service';
 import { AuthService } from '../../../common/shared/services/auth.service';
-
+import { MatMenuTrigger } from '@angular/material';
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'fw-top-bar',
@@ -11,6 +11,7 @@ import { AuthService } from '../../../common/shared/services/auth.service';
 })
 export class TopBarComponent implements OnInit , OnChanges {
 
+  @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
   currentApp: string;
   constructor(public frameworkConfigService: FrameworkConfigService ,
     private _authService: AuthService, public topBarService: TopBarService) { }
