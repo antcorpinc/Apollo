2b0cb6d9cd1c27f2029ed9f7f48e768d69b6deb6
Roni <antcorpinc@gmail.com>
2019-01-20 11:13:35 +0530

    Forgot Password Changes

---
+++ Apollo.Web/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Apollo.Web.Controllers
+{
+    public class AccountController: Controller
+    {
+        public AccountController()
+        {
+
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult ForgotPassword()
+        {
+            return View();
+        }
+    }
+}
---
+++ Apollo.Web/ViewModels/ForgotPasswordViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Apollo.Web.ViewModels
+{
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
